a2d9ec3dbdd6e66f78e75a019c1a629a373c89ea
transportDev <tsa.datacomm.balinusra@gmail.com>
2025-10-10 08:48:06 +0800

    feat: add errors.css to Vite input for improved error styling

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -14,7 +14,11 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/css/errors.css",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
     ],
